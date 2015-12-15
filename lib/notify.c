junjunk/keepalived

commit 223ee0dcd19de5cc92be8a8a452eb37f702a27ef

Remove potential race condition when setting signal handlers
There was the potential for signal_run_callback to be invoked
after calling sigaction for a signal, prior to the internal signal
handler signal_SIG***_handler and signal_SIG***_v variables being
set up.

To remove the race condition, when setting a signal handler block
the signal until the internal handlers have been fully set up.

Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

lib/signals.c
@@ -81,15 +81,37 @@ void *
 signal_set(int signo, void (*func) (void *, int), void *v)
 {
 	int ret;
+	sigset_t sset;
 	struct sigaction sig;
 	struct sigaction osig;
 
-	sig.sa_handler = signal_handler;
+	assert(func != NULL);
+
+	if (func == (void*)SIG_IGN || func == (void*)SIG_DFL) {
+		sig.sa_handler = (void*)func;
+
+		/* We are no longer handling the signal, so
+		 * clear our handlers
+		 */
+		func = NULL;
+		v = NULL;
+	}
+	else
+		sig.sa_handler = signal_handler;
 	sigemptyset(&sig.sa_mask);
 	sig.sa_flags = 0;
 #ifdef SA_RESTART
 	sig.sa_flags |= SA_RESTART;
 #endif				/* SA_RESTART */
+
+	/* Block the signal we are about to configure, to avoid
+	 * any race conditions while setting the handler and
+	 * parameter */
+	if (func != NULL) {
+		sigemptyset(&sset);
+		sigaddset(&sset, signo);
+		sigprocmask(SIG_BLOCK, &sset, NULL);
+	}
 
 	ret = sigaction(signo, &sig, &osig);
 
@@ -122,8 +144,12 @@ signal_set(int signo, void (*func) (void *, int), void *v)
 
 	if (ret < 0)
 		return (SIG_ERR);
-	else
-		return (osig.sa_handler);
+
+	/* Release the signal */
+	if (func != NULL)
+		sigprocmask(SIG_UNBLOCK, &sset, NULL);
+
+	return (void*)osig.sa_handler;
 }
 
 /* Signal Ignore */

commit 7f62f4ab6e5bb0675900aca99fd1b4257b5266cd

Fix reloading and invoking notify scripts
Commit 223ee0d introduced a problem with invoking notify scripts,
and also responding to SIGHUP. These are now resolved.

Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

lib/notify.c
@@ -25,6 +25,7 @@
 #include <syslog.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <signal.h>
 #include "notify.h"
 #include "signals.h"
 #include "logger.h"
@@ -35,6 +36,9 @@ static int
 system_call(const char *cmdline)
 {
 	int retval;
+
+	/* system() fails if SIGCHLD is set to SIG_IGN */
+	signal_set(SIGCHLD, (void*)SIG_DFL, NULL);
 
 	retval = system(cmdline);
 

lib/signals.c
@@ -92,8 +92,6 @@ signal_set(int signo, void (*func) (void *, int), void *v)
 	struct sigaction sig;
 	struct sigaction osig;
 
-	assert(func != NULL);
-
 	if (func == (void*)SIG_IGN || func == (void*)SIG_DFL) {
 		sig.sa_handler = (void*)func;
 
@@ -242,7 +240,6 @@ signal_handlers_clear(void *state)
 	signal_set(SIGCHLD, state, NULL);
 	signal_set(SIGUSR1, state, NULL);
 	signal_set(SIGUSR2, state, NULL);
-
 }
 
 void